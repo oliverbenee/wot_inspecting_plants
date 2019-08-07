99563c51c4affb87169756bcf994b3256c88d09b
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-07 16:05:59 +0200

    trying to change some things in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,9 @@ const myChartCtx = document.querySelector('#myChart')
 const temperature = []
 const humidity = []
 const time = []
+const worker_name = []
+const state = []
+const workers_assessment = []
 
 /* Fetches all the data for the chart */
 fetch('/data', {
@@ -57,8 +60,11 @@ function makeMyData (data) {
     temperature.push(c.temperature)
     humidity.push(c.humidity)
     time.push(c.time)
+    worker_name.push(c.worker_name)
+    state.push(c.state)
+    workers_assessment.push(c.workers_assessment)
   }
-  return { temperature, humidity, time }
+  return { temperature, humidity, time, worker_name, state, workers_assessment }
 }
 
 function updateMyChart (data) {
@@ -66,6 +72,9 @@ function updateMyChart (data) {
     temperature.push(c.temperature)
     humidity.push(c.humidity)
     time.push(c.time)
+    worker_name.push(c.worker_name)
+    state.push(c.state)
+    workers_assessment.push(c.workers_assessment)
     if (temperature.length >= 10) {
       temperature.shift()
       humidity.shift()
