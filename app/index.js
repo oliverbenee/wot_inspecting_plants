993d6d897e192abefd6808b6a15b1764e1825840
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-07 15:14:52 +0200

    added body-parser dependency

--- app/index.js
+++ app/index.js
@@ -1,27 +1,29 @@
-'use strict';
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
-const Tempandhums = require('../db/db').Tempandhums;
-require('./sensor');
+'use strict'
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Tempandhums = require('../db/db').Tempandhums
+require('./sensor')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
   layoutsDir: path.join(__dirname, '../views/layouts')
-}));
+}))
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, '../views'))
 
 app.use((request, response, next) => {
-  console.log(request.url);
-  next();
-});
+  console.log(request.url)
+  next()
+})
 
 app.use(express.static(path.join(__dirname, '../public')))
+
 
 /* Creates a new resource and throws an error message if there is one. */
 app.post('/homePage', (request, response, next) => {
@@ -48,57 +50,57 @@ app.post('/homePage', (request, response, next) => {
 
 /* Retrieves the state of the resource */
 app.get('/', (request, response, next) => {
-  response.redirect('/home');
-});
+  response.redirect('/home')
+})
 
 app.get('/home', (request, response, next) => {
-  response.render('homePage');
-});
+  response.render('homePage')
+})
 
 app.get('/data', (request, response, next) => {
   if (request.accepts('application/json') && !request.accepts('text/html')) {
     Tempandhums.all((err, data) => {
-      if (err) return next(err);
-      response.contentType('application/json');
-      response.end(JSON.stringify(data));
-    });
+      if (err) return next(err)
+      response.contentType('application/json')
+      response.end(JSON.stringify(data))
+    })
   }
-});
+})
 
 app.get('/dataC', (request, response, next) => {
   if (request.accepts('application/json') && !request.accepts('text/html')) {
     Tempandhums.getLChart((err, data) => {
-      if (err) return next(err);
-      response.contentType('application/json');
-      response.end(JSON.stringify(data));
-    });
+      if (err) return next(err)
+      response.contentType('application/json')
+      response.end(JSON.stringify(data))
+    })
   }
-});
+})
 
 app.get('/dataT', (request, response, next) => {
   if (request.accepts('application/json') && !request.accepts('text/html')) {
     Tempandhums.getLTable((err, data) => {
-      if (err) return next(err);
-      response.contentType('application/json');
-      response.end(JSON.stringify(data));
-    });
+      if (err) return next(err)
+      response.contentType('application/json')
+      response.end(JSON.stringify(data))
+    })
   }
-});
+})
 
 app.get('/requestM', (request, response, next) => {
   Tempandhums.request(request.query.date, (err, data) => {
-    if (err) return next(err);
-    response.contentType('application/json');
-    response.end(JSON.stringify(data));
-  });
-});
+    if (err) return next(err)
+    response.contentType('application/json')
+    response.end(JSON.stringify(data))
+  })
+})
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send(err.toString());
-});
+  console.log(err)
+  res.status(500).send(err.toString())
+})
 
 app.listen(port, (err) => {
-  if (err) return console.error(`An error has occured: ${err}`);
-  console.log(`Listening on http://localhost:${port}/`);
-});
+  if (err) return console.error(`An error has occured: ${err}`)
+  console.log(`Listening on http://localhost:${port}/`)
+})
