82d35a292dfd2947ea8a84d8bd3bae1d72cb3f84
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-14 00:16:55 +0200

    trying to see what happens if the values from the form aren't there

--- db/db.js
+++ db/db.js
@@ -68,7 +68,7 @@ class Tempandhums {
       // FØR COMMIT AUGUST 13, 2019 klokken 14:41 - brugte '' i stedet for backticks.
       // commit d. 13. august 2019 klokken 13:50 brugte '' rundt om worker_name
       const sql = 'INSERT INTO tempandhums(temperature, humidity) VALUES (?, ?)'
-      connection.query(sql, [tah.temperature, tah.humidity, tah.state], (err, results, fields) => {
+      connection.query(sql, [tah.temperature, tah.humidity], (err, results, fields) => {
         if (err) throw err
         connection.release()
       })
