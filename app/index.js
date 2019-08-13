74ab92da004b291f0753236e395e1677cfdbefaf
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-13 16:22:06 +0200

    merged sensor.js and index.js

--- app/index.js
+++ app/index.js
@@ -3,8 +3,28 @@ const path = require('path')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const Tempandhums = require('../db/db').Tempandhums
-const sensor = require('./sensor')
+
+/**
+ * CODE REQUIREMENTS FOR SENSOR
+ */
+
+//  Import temperature and humidity database
+const Tempandhums = require('../db/db').Tempandhums;
+//  Import the needed j package for the sensor
+const sensorTH = require('node-dht-sensor');
+//  11 is the model number for our sensor, 12 is the GPIO we connect to on the Pi
+sensorTH.initialize(11, 12);
+//  Import needed package for GPIO to function properly. 
+const Gpio = require('onoff').Gpio // #A
+
+//  An LED added to the RPI, GPIO port 4 is used.
+const led = new Gpio(4, 'out');
+
+
+ /**
+  * END OF CODE REQUIREMENTS FOR SENSOR.
+  */
+
 
 const app = express()
 const port = 3000
@@ -20,6 +40,73 @@ app.set('views', path.join(__dirname, '../views'))
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+
+
+
+
+
+
+
+/**
+ * CODE REQUIREMENTS FOR SENSOR.
+ */
+
+/*
+  Reads sensor values. Readout contains two values:
+    - temperature
+    - humidity
+  
+  Temperature and humidity is stored as a constant "tempandhumsData".
+  Values are printed to the terminal to be read.
+  For confirmation, an LED connected to the RPI blinks twice. This LED is connected to pin 4.
+*/
+
+function read () {
+  //  read the sensor values
+  let readout = sensorTH.read();
+  //  readout contains two values: temperature and humidity, which will be used
+  const tempandhumsData = {
+    temperature: readout.temperature.toFixed(2),
+    humidity: readout.humidity.toFixed(2),
+  };
+  console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+ 'humidity: ' + readout.humidity.toFixed(2) + '%');
+  
+  //  An LED on the RPI will blink twice for confirmation.
+  for (let i = 0; i <= 2; i++){
+    const interval = setInterval(() => {
+      led.writeSync(1);
+    }, 1000);
+    const interval1 = setInterval(() => {
+      led.writeSync(0);
+    }, 1000);
+  }
+  // Insert temperature and humidity data - maybe needs to be deleted.
+  Tempandhums.insert(tempandhumsData);
+
+  console.log('LED blinks twice to signal, that data has been stored.');
+}
+
+//  Listen to the event triggered on CTRL+C, if it get triggered, Cleanly close the GPIO pin before exiting
+process.on('SIGINT', () => {
+  clearInterval(interval);
+  console.log('Bye, bye!');
+  process.exit();
+});
+
+/**
+ * END OF CODE REQUIREMENTS FOR SENSOR.
+ */
+
+
+
+
+
+
+
+
+
 
 /* May not be necessary */
 app.use((request, response, next) => {
@@ -42,8 +129,8 @@ app.get('/home', (request, response, next) => {
 /* Creates a new resource and throws an error message if there is one. */
 app.post('/home', (request, response, next) => {
   const tempandhums = {
-    temperature: sensor.tempandhumsData.temperature,     //Tempandhums.tempandhumsData.temperature
-    humidity: sensor.tempandhumsdata.humidity,           //Tempandhums.tempandhumsData.humidity
+    temperature: tempandhumsData.temperature,     //Tempandhums.tempandhumsData.temperature
+    humidity: tempandhumsdata.humidity,           //Tempandhums.tempandhumsData.humidity
     worker_name: 'mynameisjeff',                              //request.body.worker_name,
     state: 2,                                                 //request.body.state,
     workers_assessment: 'the plant looks good'                //request.body.workers_assessment
