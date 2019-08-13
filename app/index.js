1866aaf39658d1c0d4e92cdc73f55e910626ce73
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-13 19:54:33 +0200

    Revert "db is now called Tempandhums, and sensor data is fetched as tempandhumsData.temperature"
    This reverts commit 42d95f902dc1f6b1abaf5010a497b8b6c57484ec.

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const Tempandhums = require('../db/db').Tempandhums
+const Tempandhums = require('../db/db').tempandhumsData
 const sensor = require('./sensor')
 
 const app = express()
@@ -42,8 +42,8 @@ app.get('/home', (request, response, next) => {
 /* Creates a new resource and throws an error message if there is one. */
 app.post('/home', (request, response, next) => {
   const tempandhums = {
-    temperature: sensor.tempandhumsData.temperature,
-    humidity: sensor.tempandhumsData.humidity,
+    temperature: tempandhumsData.temperature,
+    humidity: tempandhumsData.humidity,
     worker_name: request.body.worker_name,
     state: request.body.state,
     workers_assessment: request.body.workers_assessment
