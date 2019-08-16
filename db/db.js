99dc3c93fb45f91593fcce64da180af52686ff5b
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-17 01:55:49 +0200

    changed value inserted

--- db/db.js
+++ db/db.js
@@ -74,7 +74,7 @@ class Tempandhums {
       // FØR COMMIT AUGUST 13, 2019 klokken 14:41 - brugte '' i stedet for backticks.
       // commit d. 13. august 2019 klokken 13:50 brugte '' rundt om name
       const sql = "INSERT INTO tempandhums(temperature, humidity, state) VALUES (?, ?, ?)"
-      connection.query(sql, [tah.temperature, tah.humidity, 2000], (err, results, fields) => {
+      connection.query(sql, [tah.temperature, tah.humidity, '2000'], (err, results, fields) => {
         if (err) throw err
         connection.release()
       })
