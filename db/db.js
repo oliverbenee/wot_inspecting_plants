0f34f2521fa963ab668b1ef7d76d94eafbf7fe22
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-18 14:24:37 +0200

    state changed to varchar

--- db/db.js
+++ db/db.js
@@ -19,7 +19,7 @@ pool.getConnection((err, connection) => {
         temperature FLOAT(4,2) NOT NULL, 
         humidity FLOAT(4,2) NOT NULL, 
         worker_name TEXT NOT NULL, 
-        state FLOAT(4,2) NOT NULL, 
+        state VARCHAR(50) NOT NULL, 
         workers_assessment TEXT NOT NULL)`, (err) => {
       if (err) throw err
     })
