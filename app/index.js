5c6fc7122898e8310a4f6b6903b6289c867ba58e
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-18 14:31:53 +0200

    changed quotes set and state is correct value

--- app/index.js
+++ app/index.js
@@ -52,7 +52,7 @@ app.post('/inspection', (request, response, next) => {
     temperature: thdata.temperature,
     humidity: thdata.humidity,
     worker_name: request.body.worker_name,
-    state: thdata.temperature+thdata.humidity,
+    state: request.body.state,
     workers_assessment: request.body.workers_assessment
   };
   // Show data to be sent in terminal
