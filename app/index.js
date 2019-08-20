c9d4c7113ce16f61d55380b11f379eda1b745749
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-20 22:23:29 +0200

    console.log excess removed

--- app/index.js
+++ app/index.js
@@ -162,5 +162,4 @@ app.listen(port, (err) => {
   if (err) return console.error(`An error has occured: ${err}`)
   console.log(`Listening on http://localhost:${port}/`)
   console.log(`This program may be closed at any time by using the key combination "CTRL + C"`)
-  console.log(`Now testing sensors to ensure, they function as intended...`)
 })
