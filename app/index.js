a5829203f2e21c9e2b27f46cff88dbf1d2b56a0e
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-20 17:37:56 +0200

    should hopefully display current date and time

--- app/index.js
+++ app/index.js
@@ -35,13 +35,16 @@ app.use(express.static(path.join(__dirname, '../public')))
  * Updates time shown on home page
  */
 
+function display_c(){
+  var refresh=1000; // Refresh rate in milli seconds
+  mytime=setTimeout('display_ct()',refresh)
+}
+  
 function display_ct() {
   var x = new Date()
-  var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getYear(); 
-  x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds();
-  document.getElementById('ct').innerHTML = x1;
+  document.getElementById('ct').innerHTML = x;
   display_c();
-   }
+}   
 
 /* 
  * Retrieves the correct page to serve
