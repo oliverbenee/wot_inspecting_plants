b2b65d4eabea6c6a6290d0bf58a9c6f9179d44ca
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-20 17:20:57 +0200

    removed updateclock function

--- app/index.js
+++ app/index.js
@@ -150,24 +150,6 @@ app.get('/requestM', (request, response, next) => {
   })
 })
 
-// Update date and time shown on 
-function updateClock() {
-  var now = new Date(), // current date
-      months = ['January', 'February', '...']; // you get the idea
-      time = now.getHours() + ':' + now.getMinutes(), // again, you get the idea
-
-      // a cleaner way than string concatenation
-      date = [now.getDate(), 
-              months[now.getMonth()],
-              now.getFullYear()].join(' ');
-
-  // set the content of the element with the ID time to the formatted string
-  document.getElementById('time').innerHTML = [date, time].join(' / ');
-
-  // call this function again in 1000ms
-  setTimeout(updateClock, 1000);
-}
-
 // If an error is produced, send it to the console.
 app.use((err, req, res, next) => {
   console.log(err)
