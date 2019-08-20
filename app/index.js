1399305a56ed87006d160dd8ad3efb7cab5d63b3
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-20 17:34:44 +0200

    onload run displayct command to show time, and keep it

--- app/index.js
+++ app/index.js
@@ -31,6 +31,18 @@ app.use((request, response, next) => {
 
 app.use(express.static(path.join(__dirname, '../public')))
 
+/*
+ * Updates time shown on home page
+ */
+
+function display_ct() {
+  var x = new Date()
+  var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getYear(); 
+  x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds();
+  document.getElementById('ct').innerHTML = x1;
+  display_c();
+   }
+
 /* 
  * Retrieves the correct page to serve
  */
