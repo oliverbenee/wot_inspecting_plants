1bda9267a7e9200f25fa622f43ae886b441fdc11
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-21 23:21:13 +0200

    undo previous commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 'use strict'
 /* global fetch Handlebars */
 const myTable = document.querySelector('#myTable')
-const currentdata = document.querySelector('#currentdata')
 const myChartCtx = document.querySelector('#myChart')
 const temperature = []
 const humidity = []
@@ -21,17 +20,6 @@ fetch('/data', {
   })
 })
 
-
-/* Update table with current temperature and humidity and chart */
-fetch('/dataH', {
-  method: 'get',
-  headers: {
-    'Accept': 'application/json'
-  } }).then((response) => {
-  response.json().then((data) => {
-    currentdata.innerHTML = Handlebars.templates.data({ dhtdata: data })
-  })
-})
 
 /* Update table and chart */
 fetch('/dataT', {
@@ -65,15 +53,6 @@ function makeMyData (data) {
     workers_assessment.push(c.workers_assessment)
   }
   return { temperature, humidity, time, worker_name, state, workers_assessment }
-}
-
-/* code for the table with current data */
-function makeMyCurrentData (data) {
-  for (let c of data) {
-    temperature.push(c.temperature)
-    humidity.push(c.humidity)
-  }
-  return { temperature, humidity }
 }
 
 function updateMyChart (data) {
