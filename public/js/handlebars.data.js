863dfc631acc4f0ef7a8b3ebf050c6130adae7ff
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-22 23:53:45 +0200

    undo previous commit

--- public/js/handlebars.data.js
+++ public/js/handlebars.data.js
@@ -31,21 +31,3 @@
   },
   'useData': true })
 })()
-
-(function (){
-  templates['dataTN'] = template({ '2': function (container, depth0, helpers, partials, data) {
-    var helper; var alias1 = depth0 != null ? depth0 : (container.nullContext || {}); var alias2 = helpers.helperMissing; var alias3 = 'function'; var alias4 = container.escapeExpression
-    return '   <tr>\r\n <td>' +
-    alias4(((helper = (helper = helpers.temperaturenow || (depth0 != null ? depth0.temperaturenow : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'temperaturenow', 'hash': {}, 'data': data }) : helper))) +
-    '</td>\r\n <td>' +
-    alias4(((helper = (helper = helpers.humiditynow || (depth0 != null ? depth0.humiditynow : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'humiditynow', 'hash': {}, 'data': data }) : helper))) +
-    '</td>\r\n </tr>\r\n';
-  },
-  'compiler': [7, '>= 4.0.0'],
-  'main': function (container, depth0, helpers, partials, data) {
-    var stack1
-
-    return ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}), (depth0 != null ? depth0.dhtdata : depth0), { 'name': 'each', 'hash': {}, 'fn': container.program(1, data, 0), 'inverse': container.noop, 'data': data })) != null ? stack1 : '')
-  },
-  'useData': true })
-})()
