ea36dca7e4309025ca9963673a808d060d15ccdd
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-22 18:28:16 +0200

    added values to temperaturenow and humiditynow

--- public/js/main.js
+++ public/js/main.js
@@ -9,8 +9,8 @@ const time = []
 const worker_name = []
 const state = []
 const workers_assessment = []
-const temperaturenow = [] // Added to be used for showing current temperature
-const humiditynow = [] // Added to be used for showing current humidity
+const temperaturenow = [2] // Added to be used for showing current temperature
+const humiditynow = [2] // Added to be used for showing current humidity
 
 /* Fetches all the data for the chart */
 fetch('/data', {
