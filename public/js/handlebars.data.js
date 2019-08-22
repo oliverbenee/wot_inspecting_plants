345a4a438ee5f1f07e43151f81f0ac8bc0a91b6f
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-22 20:11:17 +0200

    2 templates for data - one for data and one for datanow

--- public/js/handlebars.data.js
+++ public/js/handlebars.data.js
@@ -5,14 +5,14 @@
 
     return '   <tr>\r\n <td>' +
     // IF THIS ONE IS CHANGED FROM TEMPERATURE TO TEMPERATURENOW, HOMEPAGE WORKS BUT INSPECTION DOESN'T
-    alias4(((helper = (helper = helpers.temperaturenow || (depth0 != null ? depth0.temperaturenow : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'temperaturenow', 'hash': {}, 'data': data }) : helper))) +
-    '</td>\r\n <td>' +
+    // alias4(((helper = (helper = helpers.temperaturenow || (depth0 != null ? depth0.temperaturenow : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'temperaturenow', 'hash': {}, 'data': data }) : helper))) +
+    //'</td>\r\n <td>' +
     // Serves temperature 
     alias4(((helper = (helper = helpers.temperature || (depth0 != null ? depth0.temperature : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'temperature', 'hash': {}, 'data': data }) : helper))) +
     '</td>\r\n <td>' +
     // IF THIS ONE IS CHANGED FROM HUMIDITY TO HUMIDITYNOW, HOMEPAGE WORKS, BUT INSPECTION DOESN'T
-    alias4(((helper = (helper = helpers.humiditynow || (depth0 != null ? depth0.humiditynow : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'humiditynow', 'hash': {}, 'data': data }) : helper))) +
-    '</td>\r\n <td>' +
+    // alias4(((helper = (helper = helpers.humiditynow || (depth0 != null ? depth0.humiditynow : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'humiditynow', 'hash': {}, 'data': data }) : helper))) +
+    // '</td>\r\n <td>' +
     alias4(((helper = (helper = helpers.humidity || (depth0 != null ? depth0.humidity : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'humidity', 'hash': {}, 'data': data }) : helper))) +
     '</td>\r\n <td>' +
     alias4(((helper = (helper = helpers.time || (depth0 != null ? depth0.time : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'time', 'hash': {}, 'data': data }) : helper))) +
@@ -22,6 +22,25 @@
     alias4(((helper = (helper = helpers.state || (depth0 != null ? depth0.state : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'state', 'hash': {}, 'data': data }) : helper))) +
     '</td>\r\n <td>' +
     alias4(((helper = (helper = helpers.workers_assessment || (depth0 != null ? depth0.workers_assessment : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'workers_assessment', 'hash': {}, 'data': data }) : helper))) +
+    '</td>\r\n </tr>\r\n';
+  },
+  'compiler': [7, '>= 4.0.0'],
+  'main': function (container, depth0, helpers, partials, data) {
+    var stack1
+
+    return ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}), (depth0 != null ? depth0.dhtdata : depth0), { 'name': 'each', 'hash': {}, 'fn': container.program(1, data, 0), 'inverse': container.noop, 'data': data })) != null ? stack1 : '')
+  },
+  'useData': true })
+
+
+
+  templates['datanow'] = template({ '1': function (container, depth0, helpers, partials, data) {
+    var helper; var alias1 = depth0 != null ? depth0 : (container.nullContext || {}); var alias2 = helpers.helperMissing; var alias3 = 'function'; var alias4 = container.escapeExpression
+
+    return '   <tr>\r\n <td>' +
+    alias4(((helper = (helper = helpers.temperaturenow || (depth0 != null ? depth0.temperaturenow : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'temperaturenow', 'hash': {}, 'data': data }) : helper))) +
+    '</td>\r\n <td>' +
+    alias4(((helper = (helper = helpers.humiditynow || (depth0 != null ? depth0.humiditynow : depth0)) != null ? helper : alias2), (typeof helper === alias3 ? helper.call(alias1, { 'name': 'humiditynow', 'hash': {}, 'data': data }) : helper))) +
     '</td>\r\n </tr>\r\n';
   },
   'compiler': [7, '>= 4.0.0'],
