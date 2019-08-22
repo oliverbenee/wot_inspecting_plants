22e62e56925f893a8f6c3a9effd8c1746596a018
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-22 10:58:08 +0200

    added print statement

--- app/index.js
+++ app/index.js
@@ -142,6 +142,7 @@ app.get('/dataT', (request, response, next) => {
 sensor.readNow() // Start reading data
 app.get('/dataTN', (request, response, next) => {
   if (request.accepts('application/json') && !request.accepts('text/html')) {
+    console.log('fetch table for thnow')
     Thnow.getLTable((err, data) => {
       if (err) return next(err)
       response.contentType('application/json')
