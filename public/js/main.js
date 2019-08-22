ad36449da973c1a330e84b608445e6eb0fbe0e55
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-08-22 15:59:59 +0200

    : data

--- public/js/main.js
+++ public/js/main.js
@@ -51,7 +51,7 @@ setInterval(() => {
       'Accept': 'application/json'
     } }).then((response) => {
     response.json().then((data) => {
-      nowdata.innerHTML = Handlebars.templates.data({ dhtdata: temperature, humidity, time })
+      nowdata.innerHTML = Handlebars.templates.data({ dhtdata: data })
     })
   })
 }, 10000)
